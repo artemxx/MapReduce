732013f3be228b8bed38447fc3753112c485689d
Artem Garkavy <artem2906x@gmail.com>
2020-04-17 21:43:55 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/MapReduce.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MapReduce.dir/main.cpp.o"
+  "MapReduce"
+  "MapReduce.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MapReduce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
